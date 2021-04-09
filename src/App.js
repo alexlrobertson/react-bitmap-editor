0555cbe00ea86e247b0f50cddbfde8f6b20f9eea
Alex Robertson <alexleonardrobertson@gmail.com>
2021-04-09 12:33:03 -0600

    Better drawing support

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import styled from 'styled-components'
 
 const getInitialPixels = () => {
     const pixels = [];
@@ -13,29 +14,37 @@ const getInitialPixels = () => {
     return pixels;
 };
 
+const Pixel = styled.td`
+  background: ${({active}) => active ? 'black' : 'white'};
+
+  &:hover {
+    background: ${({active}) => active ? 'lightgray' : 'darkgray'};
+  }`;
+
 function App() {
     const [pixels, setPixels] = useState(getInitialPixels());
 
     return (
-        <table style={{width: 400, height: 200, border: '1px solid red'}}>
+        <table style={{width: 400, height: 200, border: '1px solid red'}} cellPadding={0}
+               cellSpacing={0}>
             <tbody>
             {pixels.map((row, rowIndex) => (
                 <tr key={rowIndex}>
                     {row.map((pixel, columnIndex) => (
-                        <td key={columnIndex} style={{background: pixel ? 'black' : 'white', padding: 0}}
-                            onClick={() => {
-                                setPixels(pixels.map((row, index) => {
-                                    if (index !== rowIndex) {
-                                        return row;
-                                    }
-                                    return row.map((pixel, index) => {
-                                        if (index !== columnIndex) {
-                                            return pixel;
-                                        }
-                                        return !pixel;
-                                    });
-                                }));
-                            }}>{pixel}</td>
+                        <Pixel key={columnIndex} active={pixel}
+                               onClick={() => {
+                                   setPixels(pixels.map((row, index) => {
+                                       if (index !== rowIndex) {
+                                           return row;
+                                       }
+                                       return row.map((pixel, index) => {
+                                           if (index !== columnIndex) {
+                                               return pixel;
+                                           }
+                                           return !pixel;
+                                       });
+                                   }));
+                               }}>{pixel}</Pixel>
                     ))}
                 </tr>
             ))}
